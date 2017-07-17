04ab04ea8415fc62627ea996ea4fffa37e8d7e84
jason-k-lee-volusion <github@leefamily.co>
2017-07-17 14:16:19 -0500

    red

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write('<body style=\'background-color: blue\'>Hello, Jason World!</body>')
+        self.response.write('<body style=\'background-color: red\'>Hello, Jason World!</body>')
 
 
 app = webapp2.WSGIApplication([
